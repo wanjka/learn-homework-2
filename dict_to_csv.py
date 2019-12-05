ff91e83a15e48d6cc4b21a6918458b27d9bedc3e
Wanja Ilichev <vantusssss@yandex.ru>
2019-12-05 07:53:38 +0300

    Домашка основная, версия 1.0

--- dict_to_csv.py
+++ dict_to_csv.py
@@ -8,13 +8,24 @@
 * Запишите содержимое списка словарей в файл в формате csv
 
 """
+import csv
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    big_data = [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'}, 
+        {'name': 'Вася', 'age': 28, 'job': 'Programmer'}, 
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+        {'name': 'Миша', 'age': 35, 'job': 'Driver'}, 
+    ]
+    with open('export.csv', 'w', encoding='utf-8', newline='') as outfile:
+        fields = ['name', 'age', 'job']
+        writer = csv.DictWriter(outfile, fields, delimiter=';')
+        for user in big_data:
+            writer.writerow(user)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
